0845eb4ecda8d313e7cf8332edbd3ec28d26656f
Fahd Rahali (Exploitation Informatique) <fahd.rahali@ins.local>
2025-01-14 09:12:07 +0100

    add books array

---
+++ app.js
@@ -0,0 +1,8 @@
+const books = [
+  { title: "Ek Samandar, Mere Andar", author: "Sanjeev Joshi" },
+  {
+    title: "Political History of Assam (1947-1971) – Volume 1",
+    author: "Dr. Rajen Saikia",
+  },
+  { title: "Ambedkar: A Life", author: "Shashi Tharoor" },
+];
