1fad3e66aa3ddda87af25d26cfd7aa23100f1712
Fahd Rahali (Exploitation Informatique) <fahd.rahali@ins.local>
2025-01-14 10:41:20 +0100

    create an add and a remove book function

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const books = [
-  { title: "Ek Samandar, Mere Andar", author: "Sanjeev Joshi" },
+  { id: 1, title: "Ek Samandar, Mere Andar", author: "Sanjeev Joshi" },
   {
+    id: 2,
     title: "Political History of Assam (1947-1971) – Volume 1",
     author: "Dr. Rajen Saikia",
   },
-  { title: "Ambedkar: A Life", author: "Shashi Tharoor" },
+  { id: 3, title: "Ambedkar: A Life", author: "Shashi Tharoor" },
 ];
+
+const addBook = (authorValue, titleValue) => {
+  const id = books.length + 1;
+  books.push({ id: id, author: authorValue, titleValue });
+  return books;
+};
+
+const removeBook = (id) => {
+  books.filter();
+  const newBooks = books.filter((book) => book.id !== id);
+  return newBooks;
+};
