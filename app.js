e138d64f98e0c4c21adf5f57a39d43658de02d8a
Fahd Rahali (Exploitation Informatique) <fahd.rahali@ins.local>
2025-01-14 14:24:27 +0100

    display books in the top part of the page

--- app.js
+++ app.js
@@ -9,13 +9,50 @@ const books = [
 ];
 
 const addBook = (authorValue, titleValue) => {
+  const newBooks = [...books];
   const id = books.length + 1;
-  books.push({ id: id, author: authorValue, titleValue });
-  return books;
+  newBooks = books.push({ id: id, author: authorValue, titleValue });
+  return newBooks;
 };
 
 const removeBook = (id) => {
-  books.filter();
-  const newBooks = books.filter((book) => book.id !== id);
+const newBooks = [...books];
+   newBooks = books.filter((book) => book.id !== id);
   return newBooks;
 };
+
+const displayBooks = () => {
+    const BooksSection = document.createElement('section');
+    books.map(book => {
+        const ul = document.createElement('ul');
+
+        const liTitle = document.createElement('li');
+        liTitle.textContent = book.title;
+        ul.append(liTitle);
+
+        const liAuthor = document.createElement('li');
+        liAuthor.textContent = book.author;
+        ul.append(liAuthor);
+
+        const button = document.createElement('button');
+        button.textContent = 'Remove'
+        const liButton = document.createElement('li');
+        liButton.append(button);
+        ul.append(liButton);
+        const p = document.createElement('p');
+        p.textContent = '_____________________________________';
+        ul.append(p);
+        BooksSection.append(ul);
+
+        document.body.insertBefore(BooksSection, document.getElementById('form'));
+
+    })
+
+}
+
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log(' onload body');
+        displayBooks();
+    })
+
+
