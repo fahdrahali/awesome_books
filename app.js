97810ee70a345f5c99543b4653f2dd83f07a41f4
Fahd Rahali (Exploitation Informatique) <fahd.rahali@ins.local>
2025-01-14 15:22:23 +0100

    create a click event add book button and display it on the book list

--- app.js
+++ app.js
@@ -8,51 +8,56 @@ const books = [
   { id: 3, title: "Ambedkar: A Life", author: "Shashi Tharoor" },
 ];
 
-const addBook = (authorValue, titleValue) => {
-  const newBooks = [...books];
+const addBook = (author, title) => {
   const id = books.length + 1;
-  newBooks = books.push({ id: id, author: authorValue, titleValue });
-  return newBooks;
+  books.push({ id: id, author: author, title });
+  displayBooks();
 };
 
 const removeBook = (id) => {
-const newBooks = [...books];
-   newBooks = books.filter((book) => book.id !== id);
-  return newBooks;
+  books.filter((book) => book.id !== id);
+  books = newBooks;
+  return books;
 };
 
 const displayBooks = () => {
-    const BooksSection = document.createElement('section');
-    books.map(book => {
-        const ul = document.createElement('ul');
+  const booksSection = document.createElement("section");
+  booksSection.id = "books";
+  books.map((book) => {
+    const ul = document.createElement("ul");
 
-        const liTitle = document.createElement('li');
-        liTitle.textContent = book.title;
-        ul.append(liTitle);
+    const liTitle = document.createElement("li");
+    liTitle.textContent = book.title;
+    ul.append(liTitle);
 
-        const liAuthor = document.createElement('li');
-        liAuthor.textContent = book.author;
-        ul.append(liAuthor);
+    const liAuthor = document.createElement("li");
+    liAuthor.textContent = book.author;
+    ul.append(liAuthor);
 
-        const button = document.createElement('button');
-        button.textContent = 'Remove'
-        const liButton = document.createElement('li');
-        liButton.append(button);
-        ul.append(liButton);
-        const p = document.createElement('p');
-        p.textContent = '_____________________________________';
-        ul.append(p);
-        BooksSection.append(ul);
+    const button = document.createElement("button");
+    button.textContent = "Remove";
+    const liButton = document.createElement("li");
+    liButton.append(button);
+    ul.append(liButton);
+    const p = document.createElement("p");
+    p.textContent = "_____________________________________";
+    ul.append(p);
+    booksSection.append(ul);
 
-        document.body.insertBefore(BooksSection, document.getElementById('form'));
+    document.body.insertBefore(booksSection, document.getElementById("form"));
+  });
+};
 
-    })
+document.getElementById("btn-add").addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  document.getElementById("books").remove();
+  addBook(title, author);
+  console.log(books);
+});
 
-}
-
-    document.addEventListener('DOMContentLoaded', () => {
-        console.log(' onload body');
-        displayBooks();
-    })
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(" onload body");
+  displayBooks();
+});
